3f625e448eaccb33511f31e649cc84bb850cee6f
Paul <paul@theapplegates.com>
2022-03-24 00:10:36 +0000

    test

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@
 // NOTE: rootNotionSpaceId is optional; set it to undefined if you don't want to
 // use it.
 export const rootNotionPageId = '067dd719a912471ea9a3ac10710e7fdf'
-export const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
+// export const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
 
 // NOTE: having this enabled can be pretty expensive as it re-generates preview
 // images each time a page is built. In a production setting, we recommend that
